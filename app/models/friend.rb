7cc065801e83052f41b18ab7d0766ec81982ad49
lgmakena <linetmakenag@gmail.com>
2023-07-24 21:01:54 +0300

    methods to return friends data

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,23 @@
 class Friend < ActiveRecord::Base
     has_many :bills
+
+    
+
+    def self.friend_balances_with_avatars
+        all.map do |friend|
+          {
+            id: friend.id,
+            full_name: friend.full_name,
+            balance: friend.balance,
+            avatar: friend.avatar 
+                    }
+        end
+      end
+    
+      def balance
+        bills_balance = bills.sum do |bill|
+          bill.who_paid == 1 ? (bill.bill_amount - bill.amount_paid) : (bill.amount_paid - bill.bill_amount)
+        end
+        bills_balance
+      end
 end
